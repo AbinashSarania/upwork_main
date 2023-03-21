21764e61551df9d79e77ee9e483c01dfd99e4b25
AbinashSarania <102679085+AbinashSarania@users.noreply.github.com>
2023-03-21 21:28:40 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -149,6 +149,24 @@ export default function HeroSection() {
                 </div>
             </div>
 
+             {/* Contact Us */}
+            <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+                <div>
+                    <Link
+                        to="/ContactUs"
+                        title="Contact us"
+                        className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
+                    >
+                        <img
+                            className="object-cover object-center "
+                            src={chatimage}
+                            width="80%"
+                            height="80%"
+                        />
+                    </Link>
+                </div>
+            </div>
+
             {/* Map Section */}
             <div>
                 <div className="flex flex-col items-center justify-center bg-black">
@@ -2791,23 +2809,7 @@ export default function HeroSection() {
                 </div>
             </div>
 
-            {/* Contact Us */}
-            <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
-                <div>
-                    <Link
-                        to="/ContactUs"
-                        title="Contact us"
-                        className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
-                    >
-                        <img
-                            className="object-cover object-center "
-                            src={chatimage}
-                            width="80%"
-                            height="80%"
-                        />
-                    </Link>
-                </div>
-            </div>
+           
 
         </div>
 
