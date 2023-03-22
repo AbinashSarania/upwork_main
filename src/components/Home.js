f16fc4ec2862f5fee1e7fd3813ba6f4e52831f2f
AbinashSarania <102679085+AbinashSarania@users.noreply.github.com>
2023-03-22 15:30:30 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -113,16 +113,16 @@ export default function HeroSection() {
             {/* Image Grid Section */}
             <div>
                 <div className="flex flex-col md:flex-row justify-center mt-32 mb-32">
-                    <div className="grid grid-cols-1 justify-center md:grid-cols-3 gap-0 mx-8">
+                    <div className="grid grid-cols-1 gap-0 mx-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
                         {posts.map((item, key) => (
                             <div className={`w-full lg:max-w-xs ${item.className}`} key={key}>
                                 <div className="p-4">
-                                    <h4 className={`text-2xl font-semibold text-black text-center h-0 mb-8 ${key === 0 ? 'font-bold font-WorkSans text-xl mt-16' : ''}`}>
+                                    <h4 className={`text-2xl font-semibold text-black text-center h-0 mb-8 ${key === 0 ? 'font-bold font-WorkSans text-xl mt-8 mb-6' : ''}`}>
                                         {item.title}
                                     </h4>
                                 </div>
                                 {item.isTextOnly ? (
-                                    <p className={`p-4 text-justify md:text-center sm:text-sm sm:leading-snug md:leading-relaxed lg:leading-relaxed ${key === 0 ? 'text-lg mt-2 mb-8' : ''}`}>
+                                    <p className={` text-black text-center h-0 mb-8 ${key === 0 ? ' font-WorkSans text-sm mx-2 mt-8 mb-8' : ''}`}>
                                         {item.content}
                                     </p>
                                 ) : (
@@ -139,7 +139,7 @@ export default function HeroSection() {
                                     </div>
                                 )}
                                 {!item.isTextOnly && (
-                                    <p className="p-4 text-justify  sm:text-sm sm:leading-snug md:leading-relaxed lg:leading-relaxed font-WorkSans">
+                                    <p className={`p-4 text-center sm:text-sm sm:leading-snug md:leading-relaxed lg:leading-relaxed max-w-xs mx-auto ${key === 0 ? 'text-lg mt-2 mb-8' : ''}`}>
                                         {item.content}
                                     </p>
                                 )}
@@ -149,7 +149,7 @@ export default function HeroSection() {
                 </div>
             </div>
 
-             {/* Contact Us */}
+            {/* Contact Us */}
             <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
                 <div>
                     <Link
@@ -2809,7 +2809,7 @@ export default function HeroSection() {
                 </div>
             </div>
 
-           
+
 
         </div>
 
